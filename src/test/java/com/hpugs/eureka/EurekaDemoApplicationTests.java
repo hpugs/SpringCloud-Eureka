b6a50315c20dad8318785a10acfe245ef1560db2
xinge <ai_hpugs@163.com>
2024-11-14 20:22:14 +0800

    first commit

---
+++ src/test/java/com/hpugs/eureka/EurekaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpugs.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
